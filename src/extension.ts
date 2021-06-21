7449d2d8e67b980118a8c9634ed96827e73f9fe6
西索酱 <405773654@qq.com>
2021-06-21 19:39:21 +0800

    合并显示文件

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
-import { LuaAPI } from './lua_api_service';
+import { API } from './api_service';
 import { loadLocale } from './utils';
-import { JavascriptAPI } from './js_api_service';
 import { PanoramaCSS } from './panorama_css';
 import { AbilitiesDataDriven } from './abilities_data_driven';
 import { CustomGameDocuments } from './documents_service';
@@ -10,11 +9,9 @@ import { CustomGameSettingsProvider } from './editors/custom_game_settings';
 
 export function activate(context: vscode.ExtensionContext) {
     loadLocale();
-    context.subscriptions.push(new LuaAPI(context, false, 'en').register());
-    context.subscriptions.push(new LuaAPI(context, false, 'zh-cn').register());
-    context.subscriptions.push(new LuaAPI(context, true, 'en').register());
-    context.subscriptions.push(new LuaAPI(context, true, 'zh-cn').register());
-    context.subscriptions.push(new JavascriptAPI(context).register());
+    context.subscriptions.push(new API(context, 'js').register());
+    context.subscriptions.push(new API(context, 'lua_server').register());
+    context.subscriptions.push(new API(context, 'lua_client').register());
     context.subscriptions.push(new PanoramaCSS(context).register());
     context.subscriptions.push(new AbilitiesDataDriven(context).register());
     context.subscriptions.push(new CustomGameDocuments(context).register());
